38f0ed8bed7121cd22ea752122c48c2d458fdcc9
Ash Vince <amvince@gmail.com>
2019-01-24 21:42:03 -0800

    Cleanup

--- Tank Drive/src/main/java/frc/robot/commands/ArmDown.java
+++ Tank Drive/src/main/java/frc/robot/commands/ArmDown.java
@@ -7,11 +7,9 @@ import frc.robot.Robot;
 public class ArmDown extends Command {
 
   public ArmDown() {
-    // Use requires() here to declare subsystem dependencies
     requires(Robot.m_arm);
   }
 
-  // Called just before this Command runs the first time
   @Override
   protected void initialize() {
      {
@@ -19,25 +17,20 @@ public class ArmDown extends Command {
     }
   }
 
-  // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
   }
 
-  // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
     return false;
   }
 
-  // Called once after isFinished returns true
   @Override
   protected void end() {
     Robot.m_arm.stop();
   }
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
   @Override
   protected void interrupted() {
     end();  }
--- Tank Drive/src/main/java/frc/robot/commands/ArmUp.java
+++ Tank Drive/src/main/java/frc/robot/commands/ArmUp.java
@@ -7,11 +7,9 @@ import frc.robot.Robot;
 public class ArmUp extends Command {
 
   public ArmUp() {
-    // Use requires() here to declare subsystem dependencies
     requires(Robot.m_arm);
   }
 
-  // Called just before this Command runs the first time
   @Override
   protected void initialize() {
     if (Robot.m_arm.isLimit()) { 
@@ -19,25 +17,20 @@ public class ArmUp extends Command {
     }
   }
 
-  // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
   }
 
-  // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
     return !Robot.m_arm.isLimit();
   }
 
-  // Called once after isFinished returns true
   @Override
   protected void end() {
     Robot.m_arm.stop();
   }
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
   @Override
   protected void interrupted() {
     end();  }
